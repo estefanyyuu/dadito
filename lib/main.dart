8a684eb706f8340dad0b454ac72538f19502d5c0
estefanyyuu <estefanyureta45@gmail.com>
2024-04-30 17:53:51 -0600

    hello

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:custom_app_test/gradient_container.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: const Scaffold(
       body: GradientContainer(
           [Color.fromARGB(255, 1, 57, 102), Color.fromARGB(255, 1, 114, 206)]),
     ),
